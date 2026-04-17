26a3ab824bb97eafce3df1b8dbdc7a0564288a35
SLSRK <ivans2001slesarenko@gmail.com>
2026-04-17 03:16:00 +0300

    Rollback: *

--- api-gateway/src/main/java/org/innowise/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/innowise/api_gateway/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package org.innowise.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -11,7 +11,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception{
-        /*return http
+        return http
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .formLogin(formLogin -> formLogin.disable())
@@ -24,29 +24,7 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyExchange()
                         .permitAll())*/
-                /*.authorizeExchange(ex -> ex.anyExchange().permitAll())
-                .build();*/
-        return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .authorizeExchange(ex -> ex
-                        .pathMatchers(
-                                "/authservice/auth/login",
-                                "/authservice/auth/register",
-
-                                "/authservice/v3/api-docs",
-                                "/userservice/v3/api-docs",
-                                "/orderservice/v3/api-docs",
-
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**"
-                        ).permitAll()
-                        .anyExchange().authenticated()
-                )
-                /*.oauth2ResourceServer(oauth -> oauth
-                        .jwt(Customizer.withDefaults())
-                )*/
+                .authorizeExchange(ex -> ex.anyExchange().permitAll())
                 .build();
     }
 }
--- api-gateway/src/main/java/org/innowise/api_gateway/security/JwtFilter.java
+++ api-gateway/src/main/java/org/innowise/api_gateway/security/JwtFilter.java
@@ -1,24 +1,12 @@
 package org.innowise.api_gateway.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
 
 @Component
-public class JwtFilter implements GlobalFilter, Ordered {
+public class JwtFilter {
 
-    /*private boolean validateToken(String token) {
+    private boolean validateToken(String token) {
         WebClient client = WebClient.create("http://localhost:8083");
 
         return Boolean.TRUE.equals(
@@ -29,58 +17,5 @@ public class JwtFilter implements GlobalFilter, Ordered {
                         .bodyToMono(Boolean.class)
                         .block()
         );
-    }*/
-
-    @Value("${jwt.secret}")
-    private String secret;
-
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-
-        String path = exchange.getRequest().getURI().getPath();
-
-        if (path.contains("/auth/login") ||
-                path.contains("/auth/register") ||
-                path.contains("/v3/api-docs") ||
-                path.contains("/swagger-ui")) {
-
-            return chain.filter(exchange);
-        }
-
-        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            return unauthorized(exchange);
-        }
-
-        String token = authHeader.substring(7);
-
-        try {
-            Claims claims = Jwts.parser()
-                    .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
-                    .build()
-                    .parseSignedClaims(token)
-                    .getPayload();
-
-            ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
-                    .header("X-User-Id", claims.getSubject())
-                    .header("X-User-Role", (String) claims.get("role"))
-                    .build();
-
-            return chain.filter(exchange.mutate().request(mutatedRequest).build());
-
-        } catch (Exception e) {
-            return unauthorized(exchange);
-        }
-    }
-
-    private Mono<Void> unauthorized(ServerWebExchange exchange) {
-        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-        return exchange.getResponse().setComplete();
-    }
-
-    @Override
-    public int getOrder() {
-        return -1;
     }
 }
